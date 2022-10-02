45a1b49def752fd3114fbc066cb1097f3691747a
Ojasvi1329 <patel.oj@northeastern.edu>
2022-10-01 21:22:04 -0400

    product class

---
+++ AED/src/model/product.java
@@ -0,0 +1,111 @@
+package model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class product {
+    private String name;
+    private int employeeid;
+    private int age;
+    private String gender;
+    private String startdate;
+    private String level;
+    private String teaminfo;
+    private String positiontitle;
+    private String cellphone_no;
+    private String emailid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(int employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return positiontitle;
+    }
+
+    public void setPositiontitle(String positiontitle) {
+        this.positiontitle = positiontitle;
+    }
+
+    public String getCellphone_no() {
+        return cellphone_no;
+    }
+
+    public void setCellphone(String cellphone_no) {
+        this.cellphone_no = cellphone_no;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+    
+    
+    
+   
+    
+    
+    
+    
+}
---
+++ AED/src/model/producthistory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.Arraylist;
+
+/**
+ *
+ * @author HP
+ */
+public class producthistory {
+    private Arraylist<product>history;
+    
+}
