6f1c71a603f0cd700ac9800e9363e62b58bfe6f0
Ojasvi1329 <patel.oj@northeastern.edu>
2022-10-02 21:28:48 -0400

    crud file commits

---
+++ CRUDfile/src/crudfile/CRUDfile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crudfile;
+
+/**
+ *
+ * @author HP
+ */
+public class CRUDfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Createfile cr = new Createfile();
+        cr.show();
+    }
+    
+}
